23d7a3d967c0ea7636baa5cdd729059d03443d00
Aditya Patange <contact.adityapatange@gmail.com>
2024-04-18 14:39:56 +0530

    add header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,21 +6,5 @@ import Profile from "@/components/profile";
 export default async function Home() {
   const session = await auth();
 
-  return (
-    <div>
-      <form className="p-4" action={actions.signIn}>
-        <Button type="submit">Sign Up!</Button>
-      </form>
-      <form className="p-4" action={actions.signOut}>
-        <Button type="submit">Sign Out!</Button>
-      </form>
-      {session?.user ? (
-        <p>{JSON.stringify(session.user)}</p>
-      ) : (
-        <p>You are signed out!</p>
-      )}
-      <h1>Profile</h1>
-      <Profile />
-    </div>
-  );
+  return <div>Home Page</div>;
 }
